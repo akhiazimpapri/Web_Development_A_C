4ff9a0b06f4ebba5c9a4be1489ca090522777be9
Akhi Azim Papri <akhiazimru@gmail.com>
2024-09-07 11:14:39 +0600

    JS class2 PQ1

---
+++ JavaScript/app.js
@@ -0,0 +1,14 @@
+let color = "green";
+
+//Traffic light system.
+if(color === "red") {
+    console.log("stop! Light color is red.");
+}
+
+if(color === "yellow") {
+    console.log("slow down! Light color is yellow.");
+}
+
+if(color === "green") {
+    console.log("go! Light color is green.");
+}
