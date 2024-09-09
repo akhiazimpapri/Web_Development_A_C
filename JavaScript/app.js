402b0dbfa9c4c9f6cd27b9a94c1307e8b2c5e71b
Akhi Azim Papri <akhiazimru@gmail.com>
2024-09-09 09:26:01 +0600

    JS class3 Ps.Q2

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,21 +1,9 @@
-// QS1
+let start = ['january', 'july', 'march', 'august'];
 
-let msg = "help!";
-console.log(msg.trim().toUpperCase());
+start.shift();
+start.shift();
+start.unshift("june");
+start.unshift("july");
 
-// QS2
+console.log(start);
 
-let urname = "ApnaCollege";
-
-let str1 = urname.slice(4,9);
-let str2 = urname.indexOf("na");
-let str3 = urname.replace("Apna","Our");
-
-console.log(str1);
-console.log(str2);
-console.log(str3);
-
-// Qs 3
-
-let str4 = urname.slice(4,11);
-console.log(str4.replace("l","t"));
