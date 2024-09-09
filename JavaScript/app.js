e40aed4b6c9c3418b1e319f8402f6f5c4dfd531f
Akhi Azim Papri <akhiazimru@gmail.com>
2024-09-09 10:19:41 +0600

    JS class3 Ps.Q3

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,9 +1,16 @@
+//QS 1
 let start = ['january', 'july', 'march', 'august'];
 
-start.shift();
-start.shift();
-start.unshift("june");
-start.unshift("july");
+start.splice(0,2);
+start.splice(0,0, "july", "june");
 
 console.log(start);
 
+
+//QS 2
+
+let language = ["c", "c++", "html", "javascript", "python", "java", "c#", "sql"];
+
+language.reverse();
+
+console.log(language.indexOf("javascript"));
