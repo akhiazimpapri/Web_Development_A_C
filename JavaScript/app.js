18059b284267e8d4a693e34467d30b9dfec51aa0
Akhi Azim Papri <akhiazimru@gmail.com>
2024-09-09 11:25:12 +0600

    JS class3 Ps.Q4

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,16 +1,3 @@
-//QS 1
-let start = ['january', 'july', 'march', 'august'];
+let game = [['X',null,'O'], [null,'X',null], ['O',null,'X']];
 
-start.splice(0,2);
-start.splice(0,0, "july", "june");
-
-console.log(start);
-
-
-//QS 2
-
-let language = ["c", "c++", "html", "javascript", "python", "java", "c#", "sql"];
-
-language.reverse();
-
-console.log(language.indexOf("javascript"));
+console.log(game);
