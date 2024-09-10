ce2647ce93bc342d1c884cfdc463a008fde623ee
Akhi Azim Papri <akhiazimru@gmail.com>
2024-09-10 20:48:58 +0600

    JS class4 Assignment

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,28 +1,56 @@
-let  todo = [];
-let req = prompt("please enter your request");
+//QS 1
+let arr = [1,2,3,4,5,6,2,3];
 
-while(true) {
+let num = 2;
 
-    if(req == "list") {
-        console.log("--------------");
-        for(let i=0; i<todo.length; i++) {
-            console.log(i, todo[i]);
-        }
-        console.log("--------------");
-    } else if(req == "add") {
-        let task = prompt("Please enter the task you want to add");
-        todo.push(task);
-        console.log("task added");
-    } else if(req == "delete") {
-        let indx = prompt("please enter the task index");
-        todo.splice(indx, 1);
-        console.log("task deleted");
-    } else if(req == "quit") {
-        console.log("quitting app");
-        break;
-    } else {
-        console.log("wrong request");
+for(let i=0; i<arr.length; i++) {
+    if(arr[i] == num) {
+        arr.splice(i, 1);
     }
-
-    req = prompt("please enter your request"); 
 }
+
+//QS 2 
+let number = 287152;
+let count = 0;
+let copy = number;
+ while(copy > 0) {
+    count++;
+    copy = Math.floor(copy/10);
+}
+console.log(count);
+
+
+//QS 3
+let numberr = 287152;
+let sum = 0;
+let copyy = numberr;
+
+ while(copyy > 0) {
+    digit = copyy % 10;
+    sum = sum + digit;
+    copy = Math.floor(copy/10);
+}
+console.log(sum);
+
+//QS 4
+let numb = 7;
+let fact = 1
+while(numb > 0) {
+    fact = fact * numb;
+    numb--;
+}
+console.log(fact);
+
+//QS 5
+
+let array = [2, 5, 10, 4, 2, 7, 1, 9];
+
+let largest = 0;
+
+for(let i=0; i<array.length; i++) {
+    if(largest < array[i]) {
+        largest = array[i];
+    }
+}
+
+console.log(largest);
