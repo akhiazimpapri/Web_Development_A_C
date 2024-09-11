c3865f670e39a878a008fa72004d568df9713029
Akhi Azim Papri <akhiazimru@gmail.com>
2024-09-11 21:57:32 +0600

    JS class5 assignment

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,18 +1,25 @@
-let max = prompt("Enter a maximum number:");
-let guess = prompt(`Guess a random number between 1 to ${max}:`);
-let random = Math.floor(Math.random() * max) + 1;
+//QS 1
+let random = Math.floor(Math.random() * 6) + 1;
 
-while(true) {
-    if(guess === "quit") {
-        console.log("You quited");
-        break;
-    } else if(guess === random) {
-        console.log("You have guess the right number! congrats!!");
-        break;
-    } else if(guess < random) {
-        guess = prompt("Your guess was too small.Try again.");
-    } else if(guess > random) {
-        guess = prompt("Your guess was too large.Try again.");
-    } 
-}
+console.log(random);
 
+
+//QS 2
+let car = {
+    name: 'suv',
+    model: 2005,
+    color: 'red'
+};
+
+console.log(car.name);
+
+//QS 3
+let person = {
+    name: "Akhi Azim Papri",
+    age: 24,
+    city: "Rajshahi"
+};
+
+person.city = "New York";
+person.country = "United States";
+console.log(person);
