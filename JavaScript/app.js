4437a3a3aa97084b6892086ffd27731ca0aa0b28
Akhi Azim Papri <akhiazimru@gmail.com>
2024-09-13 21:33:27 +0600

    JS class6 PQ2

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,29 +1,5 @@
-function printPoem() {
-    console.log("The Raven");
-
- console.log("by Edgar Allen Poe");
-
- console.log("Deep into that darkness peering,");
-
- console.log("Long I stood there, wondering, fearing,");
-
- console.log("Doubting, dreaming dreams no mortals");
-
- console.log("Ever dared to dream before;");
-
- console.log("But the silence was unbroken,");
-
- console.log("And the stillness gave no token,");
-
- console.log("And the only word there spoken");
-
- console.log('Was the whispered word, "Lenore!"');
-
- console.log("This I whispered, and an echo");
-
- console.log('Murmured back the word, "Lenore!"');
-
- console.log("Merely this, and nothing more.");
+function dice() {
+    console.log(Math.floor(Math.random() * 6) + 1);
 }
 
-printPoem();
+dice();
