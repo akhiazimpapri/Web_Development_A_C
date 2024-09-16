fdb0a1d0af9a2fd0b3a50e204549f5385e4537e8
Akhi Azim Papri <akhiazimru@gmail.com>
2024-09-16 10:56:25 +0600

    JS class7 arrow function

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,73 +1,13 @@
-//QS 1
-let arr = [1,2,3,4,5,6,7,8,9,10];
-let num = 6;
-
-function longer(arr, n) {
-    for(let i = 0; i < arr.length; i++) {
-        if(arr[i] > n) {
-            console.log(arr[i]);
-        }
-    }
+//arrow function
+const sum = (a,b) => { 
+    console.log(a+b);
 }
 
-longer(arr, num);
-
-//QS 2
-let str = "abcdabcdefgggh";
-
-function uniqueChar(str) {
-    let ans = '';
-    for(i = 0; i < str.length; i++) {
-        let currChar = str[i];
-        if(ans.indexOf(currChar) === -1) {
-            ans = ans + currChar;
-        }
-
-    }
-    return ans;
+const cube = (a) => {
+    return a * a * a; 
 }
 
-console.log(uniqueChar(str));
-
-//QS 3
-let country = ["Australia", "Germany", "United States of America"];
-
-function longestCountry(country) {
-    let ansIdx = 0;
-    for(let i=0; i<country.length; i++) {
-        let ansLen = country[ansIdx].length;
-        let currLen = country[i].length;
-        if(currLen > ansLen) {
-            ansIdx = i;
-        }
-    }
-    return country[ansIdx];
-}
-
-console.log(longestCountry(country));
-
-//QS 4
-
-let stri = "apnacollege";
-function countVowels(stri) {
-    let count = 0;
-    for (let i = 0; i < stri.length; i++) {
-        if (stri.charAt(i) == "a" || stri.charAt(i) == "e" || stri.charAt(i) == "i" || stri.charAt(i) == "o" || stri.charAt(i) == "u") {
-            count++;
-        }
-    }
-return count;
-}
-
-console.log(countVowels(stri));
-
-//QS 5
-let start = 100;
-let end = 200;
-
-function randomNum(start, end) {
-    let diff = end - start;
-    return Math.floor(Math.random() * diff) + start;
-}
-
-console.log(randomNum(start, end));
+//it can be writen as like this when a simple work is done.
+const mul = (a,b) => (
+    a * b
+);
